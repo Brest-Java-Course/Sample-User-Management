1b38e20148ec2a20aa2ed15b190b6e6436a7200b
Aliaksandr-Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2014-10-20 21:47:19 +0300

    DAO module

---
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -0,0 +1,26 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by mentee-42 on 20.10.14.
+ */
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public void removeUser(Long userId) {
+
+    }
+}
