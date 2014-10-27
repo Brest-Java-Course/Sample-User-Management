98e3359ac3e5a33312972e44e146d1d5521de6bc
Aliaksandr-Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2014-10-27 21:38:00 +0300

    easymock

---
+++ app-service/src/test/java/com/epam/brest/courses/service/UserDataFixture.java
@@ -0,0 +1,24 @@
+package com.epam.brest.courses.service;
+
+import com.epam.brest.courses.domain.User;
+
+/**
+ * Created by mentee-42 on 27.10.14.
+ */
+public class UserDataFixture {
+
+    public static User getNewUser() {
+        User user = new User();
+        user.setName("name");
+        user.setLogin("login");
+        return user;
+    }
+
+    public static User getExistUser(long id) {
+        User user = new User();
+        user.setUserId(id);
+        user.setName("name");
+        user.setLogin("login");
+        return user;
+    }
+}
